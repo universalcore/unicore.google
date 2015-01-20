b34479fe81196edbaa8bd0b64a8bca0766f1d485
Simon de Haan <simon@praekeltfoundation.org>
2015-01-20 14:48:13 +0200

    test

--- unicore/google/tests/test_tasks.py
+++ unicore/google/tests/test_tasks.py
@@ -1,8 +1,7 @@
 from unittest import TestCase
+from urlparse import parse_qs
 
 from mock import patch
-
-from unicore.google.tasks import pageview
 
 
 class TestTasks(TestCase):
@@ -10,11 +9,31 @@ class TestTasks(TestCase):
     @patch('urllib2.urlopen')
     def test_pageview(self, mock_request):
         mock_request.return_value = True
-        pageview('profile_id', 'client_id', {
+
+        from unicore.google.tasks import pageview
+        pageview('profile-id', 'client-id', {
             'path': 'foo',
             'user_agent': 'user agent',
             'uip': 'the-ip',
             'dr': 'referrer',
             'dh': 'domain',
         })
-        mock_request.assert_called_with('foo')
+        mock_request.assert_called()
+        (args, kwargs) = mock_request.call_args
+        (request,) = args
+
+        self.assertEqual(request.headers, {
+            'User-agent': 'user agent',
+        })
+
+        data = parse_qs(request.data)
+        self.assertEqual(data, {
+            'dh': ['domain'],
+            'cid': ['client-id'],
+            'uip': ['the-ip'],
+            'v': ['1'],
+            't': ['pageview'],
+            'tid': ['profile-id'],
+            'dr': ['referrer'],
+            'dp': ['foo'],
+        })
